7e0e341f391d844672d134c0d7a0d23cbbd3a8f9
arseniy-grinev <arseniy.grinev@gmail.com>
2022-08-24 12:25:30 +0300

    end tsk2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from '../js/gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// import galleryTemplate from '../templates/gallery-items.hbs';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
+function createGalleryMarkup(items) {
+    return items.map(({ description, original, preview }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        `;
+    }).join('');   
+}
+
+
+
+
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,22 @@
+import  Player  from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const TIME_K = 'videoplayer-current-time';
 
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-var throttle = require('lodash.throttle');
+player.on('timeupdate', throttle(onPlay, 1000));
+
+if (localStorage.getItem(TIME_K)) {
+    setCurrentTime();
+}
+
+function onPlay({ seconds }) {
+     localStorage.setItem(TIME_K, seconds)
+}
+
+function setCurrentTime() {
+    player.setCurrentTime(localStorage.getItem(TIME_K))
+}
